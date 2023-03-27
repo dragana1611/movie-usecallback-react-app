ee58d764759d8402c5d52b1b948d4a0dc784f1b0
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-03-27 19:51:35 +0200

    first commit

---
+++ build/precache-manifest.41839dafe639d9b5d3085fabbac59c54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97ca854f8402168d88ba84651f1888c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b313a9992e3a607947be",
+    "url": "/static/css/main.1b4620d7.chunk.css"
+  },
+  {
+    "revision": "c4e0afda0f838c38e0a3",
+    "url": "/static/js/2.07c3fd25.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.07c3fd25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b313a9992e3a607947be",
+    "url": "/static/js/main.11304304.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.11304304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea5b715b92fd6d69b18",
+    "url": "/static/js/runtime-main.7ab1f2ce.js"
+  }
+]);
